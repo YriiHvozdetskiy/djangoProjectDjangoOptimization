f5380962e755c0348ef24f1cb8366e0046abbcdd
rasty <hvozdetskiyyrii@gmail.com>
2024-07-04 19:32:31 +0300

    optimizations SQL

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
-from services.models import Subscription
+from services.models import Subscription, Plan
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = '__all__'
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer()
     client_name = serializers.CharField(source='client.company_name')  # client.company_name - бачити яка компанія
     email = serializers.CharField(source='client.user.email')
 
@@ -15,4 +22,4 @@ class SubscriptionSerializer(serializers.ModelSerializer):
          plan_id - скрито в plan = models.ForeignKey(Plan, on_delete=models.PROTECT, related_name='subscriptions')
          але ми можем до нього звитратися тут.
         """
-        fields = ('id', 'plan_id', 'client_name', 'email')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan')
--- service/services/views.py
+++ service/services/views.py
@@ -1,9 +1,32 @@
+from django.db.models import Prefetch
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from services.models import Subscription
+from services.models import Subscription, Plan
 from services.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    # client__user - див пояснення в Notions
+    # select_related: для "прямих" зв'язків (ForeignKey, OneToOne),автоматично створює JOIN.
+    # prefetch_related: для "зворотних" зв'язків і ManyToMany.
     serializer_class = SubscriptionSerializer
+    queryset = Subscription.objects.all()
+
+    def get_queryset(self):
+        return Subscription.objects.select_related(
+            'plan',
+            'client__user'
+        ).prefetch_related(
+            Prefetch(
+                'plan',
+                queryset=Plan.objects.all(),
+                to_attr='prefetched_plan'
+            )
+        ).only(
+            'id',
+            'client__company_name',
+            'client__user__email',
+            'plan__id',
+            'plan__plan_type',
+            'plan__discount_percent'
+        )
