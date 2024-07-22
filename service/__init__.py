581025eb8d34b247c86b1bddf266c31819ad6bce
rasty <hvozdetskiyyrii@gmail.com>
2024-07-22 22:57:24 +0300

    update

--- service/__init__.py
+++ service/__init__.py
@@ -1,4 +1,4 @@
-from celery_app import app as celery_app
+from .celery_app import app as celery_app
 
 # __all__ = ('celery_app',) - щоб наша celery_app стартувала з нашим приложенієм django
 __all__ = ('celery_app',)
